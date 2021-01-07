0234af3f0df490880221e53e4f624e57262957c2
andywar65 <andy.war1965@gmail.com>
2021-01-07 15:58:59 +0100

    added snippet model

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party
+    'rest_framework',
+    #local
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
