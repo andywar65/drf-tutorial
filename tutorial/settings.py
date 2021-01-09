f47ab65ff6b299ecddd6d309a1e4c8c98a2cc1c0
andywar65 <andy.war1965@gmail.com>
2021-01-09 09:15:22 +0100

    added pagination

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
