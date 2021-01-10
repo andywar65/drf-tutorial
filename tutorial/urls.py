8cf9e4a2f9b924923737359c57fb685a1d00f31a
andywar65 <andy.war1965@gmail.com>
2021-01-10 20:30:43 +0100

    restoring admin urls to access admin

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
